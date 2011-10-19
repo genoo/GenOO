87b26bc7ffce71f399853104fed8af70d4c15c5b
Manolis Maragkakis <em.maragkakis@gmail.gr>
2011-10-18 20:19:47 -0400

    1/ Added the .gitignore file to temporarily exclude folder "doc" from syncing.    Syncing will be enabled again when the doc format has been set and standardized 2/ Added a class named MyBio::NGS::BioFile which serves as an accessor for several file formats    used in bioinformatics. Supplementary classes are the MyBio::File::BED and MyBio::File::SAM 3/ Added the MyBio/NGS/Experiment class which serves as an accessor for the information of a    Next Generation Sequencing experiment.

--- MyBio/NGS/Experiment.pm
+++ MyBio/NGS/Experiment.pm
@@ -1,9 +1,45 @@
-=begin nd
+# POD documentation - main docs before the code
 
-Class: MyBio::NGS::Experiment
-A class that describes a next generation sequencing experiment
+=head1 NAME
+
+MyBio::NGS::Experiment - Object describing a Next Generation Sequencing experiment
+
+=head1 SYNOPSIS
+
+    # Object that offers as an information holder for NGS experiments
+    # It contains information about the samples used the type of analysis for every sample and several more attributes
+    # It primary goal is to be extensible so that it can describe several types of experiments
+    
+    # To initialize 
+    my $experiment = MyBio::NGS::Experiment->new({
+        NAME            => undef,
+        SPECIES         => undef,
+        DESCRIPTION     => undef,
+        PARAMS          => undef,
+        EXTRA_INFO      => undef,
+    });
+
+
+=head1 DESCRIPTION
+
+    This object is constructed from an xml file which contains information about a NGS experiment.
+    It contains accessors for different types of subexperiments within the main project
+
+=head1 EXAMPLES
+
+    # Read experiment info
+    my $experiment_obj = MyBio::NGS::Experiment->read_xml("params.xml);
+    
+    # Get all samples for a particular sub experiment
+    
+
+=head1 AUTHOR - Manolis Maragkakis
+
+Email em.maragkakis@gmail.com
 
 =cut
+
+# Let the code begin...
 
 package MyBio::NGS::Experiment;
 use strict;
@@ -14,7 +50,7 @@ use base qw( MyBio::_Initializable );
 sub _init {
 	my ($self,$data) = @_;
 	
-	$self->read_info($$data[0]);
+	$self->read_xml($$data{XML});
 	
 	return $self;
 }
@@ -25,10 +61,84 @@ sub _init {
 sub get_info {
 	return $_[0]->{INFO};
 }
+sub get_name {
+	return $_[0]->get_info->{'name'};
+}
+sub get_path {
+	return $_[0]->get_info->{'path'};
+}
+sub get_species {
+	return $_[0]->get_info->{'species'};
+}
+sub get_species_id {
+	return $_[0]->get_info->{'species_id'};
+}
+sub get_subexperiment_names {
+	my ($self) = @_;
+	
+	if (exists $self->get_info->{'sub_experiment'}) {
+		return keys %{$self->get_info->{'sub_experiment'}};
+	}
+	else {
+		return ();
+	}
+}
+sub get_subexperiment {
+	my ($self,$name) = @_;
+	
+	if (exists $self->get_info->{'sub_experiment'}->{$name}) {
+		return $self->get_info->{'sub_experiment'}->{$name};
+	}
+	else {
+		return {};
+	}
+}
+sub get_sample_names_for_subexperiment {
+	my ($self,$name) = @_;
+	
+	if (exists $self->get_info->{'sub_experiment'}->{$name}->{'sample'}) {
+		return keys %{$self->get_info->{'sub_experiment'}->{$name}->{'sample'}};
+	}
+	else {
+		return ();
+	}
+}
+sub get_samples_for_subexperiment {
+	my ($self,$name) = @_;
+	
+	if (exists $self->get_info->{'sub_experiment'}->{$name}->{'sample'}) {
+		return values %{$self->get_info->{'sub_experiment'}->{$name}->{'sample'}};
+	}
+	else {
+		return ();
+	}
+}
+sub get_sample {
+	my ($self,$name,$sample_name) = @_;
+	
+	if (exists $self->get_info->{'sub_experiment'}->{$name}->{'sample'}->{$sample_name}) {
+		return $self->get_info->{'sub_experiment'}->{$name}->{'sample'}->{$sample_name};
+	}
+	else {
+		return {};
+	}
+}
 
 #######################################################################
 #############################   Setters   #############################
 #######################################################################
+sub set_name {
+	$_[0]->{NAME} = $_[1] if defined $_[1];
+}
+sub set_path {
+	$_[0]->{PATH} = $_[1] if defined $_[1];
+}
+sub set_species {
+	$_[0]->{SPECIES} = $_[1] if defined $_[1];
+}
+sub set_species_id {
+	$_[0]->{SPECIES_ID} = $_[1] if defined $_[1];
+}
 sub set_info {
 	$_[0]->{INFO} = defined $_[1] ? $_[1] : {};
 }
@@ -36,17 +146,41 @@ sub set_info {
 #######################################################################
 #########################   General Methods   #########################
 #######################################################################
-sub read_info {
+sub read_xml {
 	my ($self,$filename) = @_;
 	
 	$self->set_info(XMLin($filename));
+	$self->_process_info();
+	# print Dumper($self->get_info);
 }
-sub write_info {
+sub write_xml {
 	my ($self,$filename) = @_;
 	
 	open(my $OUT,">",$filename) or die "Cannot write to file $filename $!";
 	print $OUT XMLout($self->get_info);
 	close $OUT;
+}
+sub _process_info {
+	my ($self,$filename) = @_;
+	
+	my $info = $self->get_info();
+	
+	foreach my $subexp_type (keys %{$info->{'sub_experiment'}}) {
+		if ($subexp_type eq "") {
+			delete $info->{'sub_experiment'}->{$subexp_type};
+			next;
+		}
+		$info->{'sub_experiment'}->{$subexp_type}->{'name'} = $subexp_type;
+		foreach my $sample_name (keys %{$info->{'sub_experiment'}->{$subexp_type}->{'sample'}}) {
+			if ($sample_name eq "") {
+				delete $info->{'sub_experiment'}->{$subexp_type}->{'sample'}->{$sample_name};
+				next;
+			}
+			$info->{'sub_experiment'}->{$subexp_type}->{'sample'}->{$sample_name}->{'name'} = $sample_name;
+			$info->{'sub_experiment'}->{$subexp_type}->{'sample'}->{$sample_name}->{'type'} = $subexp_type;
+		}
+
+	}
 }
 
 1;
