1350fb6ce692363ea447d97364f8047d0a62b923
Panagiotis Alexiou <palexiou@mail.med.upenn.edu>
2012-06-06 17:30:29 -0400

    Creation of JobGraph methods
    Summary:
    1) Created JobGraph::Job (Interface)
    2) Created JobGraph::Job::Generic (Generic Job)
    3) Created JobGraph::Input
    4) Created JobGraph::Output
    5) Created JobGraph::Description
    6) Wrote unit tests for all

--- MyBio/JobGraph/Job.pm
+++ MyBio/JobGraph/Job.pm
@@ -2,139 +2,101 @@
 
 =head1 NAME
 
-MyBio::NGS::Pipeline::Job - Job object, with features
+MyBio::JobGraph::Job - Interface of a concrete factory for creating Job objects
 
 =head1 SYNOPSIS
 
-    # This is the main transcript object
-    # It represents a transcript of a gene
-    
-    # To initialize 
-    my $transcript = MyBio::NGS::Pipeline::Job->new({
-        ENSTID         => undef,
-        SPECIES        => undef,
-        STRAND         => undef,
-        CHR            => undef,
-        START          => undef,
-        STOP           => undef,
-        GENE           => undef, # MyBio::Gene
-        UTR5           => undef, # MyBio::NGS::Pipeline::Job::UTR5
-        CDS            => undef, # MyBio::NGS::Pipeline::Job::CDS
-        UTR3           => undef, # MyBio::NGS::Pipeline::Job::UTR3
-        CDNA           => undef, # MyBio::NGS::Pipeline::Job::CDNA
-        BIOTYPE        => undef,
-        INTERNAL_ID    => undef,
-        INTERNAL_GID   => undef,
-        EXTRA_INFO     => undef,
-    });
+    # This class should not and cannot be instantiated. It defines the interface that the concrete factories should respect.
 
 =head1 DESCRIPTION
 
-    The Transcript class describes a transcript of a gene. It has a backreference back to the gene where it belongs
-    and contains functional regions such as 5'UTR, CDS and 3'UTR for protein coding genes. If the gene is not protein
-    coding then the CDNA region is set and the above remain undefined. It's up to the user to check if the attributes
-    are defined or not. The transcript class inherits the SplicedLocus class so it also describes a genomic region
-    where Introns and Exons are defined.
-
-=head1 EXAMPLES
-
-    my %transcripts = MyBio::NGS::Pipeline::Job->read_region_info_for_transcripts('FILE',"$database/Ensembl_release_54/CDS_and_3UTR_sequences_CLEAN.txt");
-
-=head1 AUTHOR - Manolis Maragkakis, Panagiotis Alexiou
-
-Email em.maragkakis@gmail.com, pan.alexiou@fleming.gr
+    Concrete factories should respect the interface defined here.
 
 =cut
 
 # Let the code begin...
 
-package MyBio::NGS::Pipeline::Job;
+package MyBio::JobGraph::Job;
 use strict;
 
-use base qw(MyBio::_Initializable Clone);
+our $VERSION = '1.0';
+
+sub new {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
 
 sub _init {
-	my ($self,$data) = @_;
-	
-	$self->SUPER::_init($data);
-	$self->set_input($$data{INPUT});
-	$self->set_output($$data{OUTPUT});
-	$self->set_description($$data{DESCRIPTION});
-	$self->set_log($$data{LOG});
-	
-	return $self;
-}
-
-#######################################################################
-########################   Attribute Getters   ########################
-#######################################################################
-sub get_input {
 	my ($self) = @_;
-	return $self->{INPUT};
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
 }
-sub get_output {
-	my ($self) = @_;
-	return $self->{OUTPUT};
-}
-sub get_description {
-	my ($self) = @_;
-	return $self->{DESCRIPTION};
-}
-sub get_log {
-	my ($self) = @_;
-	return $self->{LOG};
-}
-
 
 #######################################################################
 ########################   Attribute Setters   ########################
 #######################################################################
 sub set_input {
-	my ($self,$value) = @_;
-	if (defined $value) {
-		$self->{ENSTID} = $value;
-		return 0;
-	}
-	else {
-		return 1;
-	}
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
 }
 sub set_output {
-	my ($self,$value) = @_;
-	if (defined $value) {
-		$self->{OUTPUT} = $value;
-		return 0;
-	}
-	else {
-		return 1;
-	}
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
 }
 sub set_description {
-	my ($self,$value) = @_;
-	if (defined $value) {
-		$self->{DESCRIPTION} = $value;
-		return 0;
-	}
-	else {
-		return 1;
-	}
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
 }
 sub set_log {
-	my ($self,$value) = @_;
-	if (defined $value) {
-		$self->{LOG} = $value;
-		return 0;
-	}
-	else {
-		return 1;
-	}
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
 }
-
-
 #######################################################################
-#############################   Methods   #############################
+########################   Attribute Getters   ########################
 #######################################################################
-
+sub get_input {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
+sub get_output {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
+sub get_description {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
+sub get_log {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
+#######################################################################
+#########################   General Methods   #########################
+#######################################################################
+sub clean {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
+sub develop {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
+sub run {
+	my ($self) = @_;
+	my $class = ref($self) || $self;
+	die "Error:  Use of undefined Abstract Method \"".(caller(0))[3]."\" by $class\n";
+}
 
 
 1;
