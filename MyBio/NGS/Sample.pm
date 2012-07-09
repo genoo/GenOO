914aaef8b628ffcd3281966740512532f0d8dfe1
Manolis Maragkakis <em.maragkakis@gmail.com>
2012-07-09 19:25:04 -0400

    Created class to handle information for a deep sequencing sample
    Summary:
    1) Created the class MyBio::NGS::Sample which reads an XML file with information
       for a deep sequencing sample and offers methods for accessing the data

---
+++ MyBio/NGS/Sample.pm
@@ -0,0 +1,113 @@
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+MyBio::NGS::Sample - Object for a deep sequencing sample
+
+=head1 SYNOPSIS
+
+    # Instantiate
+    my $sample = MyBio::NGS::Sample->new({
+        XML            => undef,
+    });
+
+=head1 DESCRIPTION
+
+    This object is constructed from an xml file which contains information about a
+    deep sequencing sample. It contains accessors for all relevant information in the XML
+
+=head1 EXAMPLES
+
+    # Read sample xml_data
+    my $sample_name = $sample->name;
+    
+    # Print all contaminants
+    my @contaminants = $sample->contaminants;
+    print "$_\n" for @contaminants;
+    
+=cut
+
+# Let the code begin...
+
+package MyBio::NGS::Sample;
+use strict;
+use XML::Simple;
+
+use base qw( MyBio::_Initializable );
+
+sub _init {
+	my ($self,$data) = @_;
+	
+	$self->read_xml($$data{XML});
+	
+	return $self;
+}
+
+#######################################################################
+############################   Accessors  #############################
+#######################################################################
+sub xml_data {
+	my ($self) = @_;
+	return $self->{XML_DATA};
+}
+
+sub name {
+	my ($self) = @_;
+	return $self->xml_data->{'name'};
+}
+
+sub species {
+	my ($self) = @_;
+	return $self->xml_data->{'species'};
+}
+
+sub species_id {
+	my ($self) = @_;
+	return $self->xml_data->{'species_id'};
+}
+
+sub type {
+	my ($self) = @_;
+	return $self->xml_data->{'type'};
+}
+
+sub subtype {
+	my ($self) = @_;
+	return $self->xml_data->{'subtype'};
+}
+
+sub sequencer {
+	my ($self) = @_;
+	return $self->xml_data->{'sequencer'};
+}
+
+sub five_p_adaptor {
+	my ($self) = @_;
+	return $self->xml_data->{'five_p_adaptor'};
+}
+
+sub three_p_adaptor {
+	my ($self) = @_;
+	return $self->xml_data->{'three_p_adaptor'};
+}
+
+sub contaminants {
+	my ($self) = @_;
+	if (exists $self->xml_data->{'contaminant'}) {
+		return @{$self->xml_data->{'contaminant'}};
+	}
+	else {
+		return ();
+	}
+}
+
+#######################################################################
+#########################   General Methods   #########################
+#######################################################################
+sub read_xml {
+	my ($self,$filename) = @_;
+	
+	$self->{XML_DATA} = XMLin($filename, ForceArray => ['contaminant'], KeepRoot => 0, KeyAttr => []);
+}
+
+1;
