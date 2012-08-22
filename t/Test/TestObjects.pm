2a7e527e721134ea3a4cc7173448fba699bd873c
Manolis Maragkakis <em.maragkakis@gmail.com>
2012-08-22 16:55:13 -0400

    Redefined the Tag class
    Summary:
    1) Removed all UCSC specific attributes from MyBio::NGS::Tag

--- t/Test/TestObjects.pm
+++ t/Test/TestObjects.pm
@@ -5,10 +5,9 @@ use MyBio::JobGraph::Job::Input;
 use MyBio::JobGraph::Job::Output;
 use MyBio::JobGraph::Job::Description;
 
-	################################################
-	################ JobGraph ######################
-	################################################
-	
+################################################
+################ JobGraph ######################
+################################################
 sub get_testobject_MyBio_JobGraph_Job_Generic {
 	return [
 		{
@@ -112,6 +111,50 @@ sub get_testobject_MyBio_Locus {
 			STOP         => "10100",
 			SEQUENCE     => "CAATACATACGTGTTCCGGCTCTTATCCTGCATCGGAAGCTCAATCATGCATCGCACCAGCGTGTTCGTGTCATCTAGGAGGGGCGCGTAGGATAAATAA",
 			NAME         => undef,
+			EXTRA_INFO   => undef,
+		},
+	];
+}
+
+################################################
+############## MyBio::NGS::Tag #################
+################################################
+sub get_testobject_MyBio_NGS_Tag {
+	return [
+		{
+			#this is a generic object of the class
+			SPECIES      => "HUMAN",
+			STRAND       => "1",
+			CHR          => "chr9",
+			START        => "10000",
+			STOP         => "10100",
+			SEQUENCE     => "CAATACATACGTGTTCCGGCTCTTATCCTGCATCGGAAGCTCAATCATGCATCGCACCAGCGTGTTCGTGTCATCTAGGAGGGGCGCGTAGGATAAATAA",
+			NAME         => "test_locus_generic",
+			SCORE        => 0.1,
+			EXTRA_INFO   => undef,
+		},
+		{
+			#this is a generic object of the class (minus strand)
+			SPECIES      => "HUMAN",
+			STRAND       => "-1",
+			CHR          => "chr9",
+			START        => "10000",
+			STOP         => "10100",
+			SEQUENCE     => "CAATACATACGTGTTCCGGCTCTTATCCTGCATCGGAAGCTCAATCATGCATCGCACCAGCGTGTTCGTGTCATCTAGGAGGGGCGCGTAGGATAAATAA",
+			NAME         => "test_locus_generic2",
+			SCORE        => 1,
+			EXTRA_INFO   => undef,
+		},
+		{
+			#this is a generic object of the class (missing name and strand)
+			SPECIES      => "HUMAN",
+			STRAND       => undef,
+			CHR          => "chr9",
+			START        => "10000",
+			STOP         => "10100",
+			SEQUENCE     => "CAATACATACGTGTTCCGGCTCTTATCCTGCATCGGAAGCTCAATCATGCATCGCACCAGCGTGTTCGTGTCATCTAGGAGGGGCGCGTAGGATAAATAA",
+			NAME         => undef,
+			SCORE        => 0,
 			EXTRA_INFO   => undef,
 		},
 	];
