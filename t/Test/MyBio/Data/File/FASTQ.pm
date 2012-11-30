3d1a23c346ecb5cfc33757b64ebabd66c36c1f72
Panagiotis Alexiou <palexiou@mail.med.upenn.edu>
2012-11-30 15:56:17 -0500

    Added FASTQ parser and tests

---
+++ t/Test/MyBio/Data/File/FASTQ.pm
@@ -0,0 +1,93 @@
+package Test::MyBio::Data::File::FASTQ;
+use strict;
+
+use base qw(Test::MyBio);
+use Test::Moose;
+use Test::Most;
+
+#######################################################################
+################   Startup (Runs once in the begining  ################
+#######################################################################
+sub _check_loading : Test(startup => 1) {
+	my ($self) = @_;
+	use_ok $self->class;
+};
+
+#######################################################################
+#################   Setup (Runs before every method)  #################
+#######################################################################
+sub create_new_test_objects : Test(setup) {
+	my ($self) = @_;
+	
+	my $test_class = ref($self) || $self;
+	$self->{TEST_OBJECTS} = $test_class->test_objects();
+};
+
+#######################################################################
+###########################   Actual Tests   ##########################
+#######################################################################
+sub _isa_test : Test(1) {
+	my ($self) = @_;
+	
+	isa_ok $self->obj(0), $self->class, "... and the object";
+}
+
+sub file : Test(2) {
+	my ($self) = @_;
+	
+	can_ok $self->obj(0), 'file';
+	is $self->obj(0)->file, 't/sample_data/sample.fastq.gz', "... and should return the correct value";
+}
+
+sub next_record : Test(9) {
+	my ($self) = @_;
+	
+	can_ok $self->obj(0), 'next_record';
+	
+	my $record = $self->obj(0)->next_record;
+	isa_ok $record, 'MyBio::Data::File::FASTQ::Record', "... and the returned object";
+	is $record->name, 'HWUSI-EAS366_6:1:4:555#0/1',  "... and should return the correct value";
+	is $record->sequence, 'AGTTTTTTNGGACCGATTCAATGCGATC',  "... and should return the correct value";
+	is $record->quality, '?ABBBCC=%<BBBBB>BBBB>BBBBAB=',  "... and should return the correct value";
+	
+	$record = $self->obj(0)->next_record;
+	isa_ok $record, 'MyBio::Data::File::FASTQ::Record', "... and the returned object";
+	is $record->name, 'HWUSI-EAS366_6:1:4:836#0/1',  "... and should return the correct value";
+	is $record->sequence, 'TAATTCGTNAACTGGGGAAGATAATAATN',  "... and should return the correct value";
+	is $record->quality, 'BB?CCBB:%<BBCB<B=0@BBBCBCBA<%',  "... and should return the correct value";
+}
+
+#######################################################################
+##########################   Helper Methods   #########################
+#######################################################################
+sub obj {
+	my ($self, $index) = @_;
+	
+	return $self->{TEST_OBJECTS}->[$index];
+}
+
+sub objs {
+	my ($self) = @_;
+	
+	return @{$self->{TEST_OBJECTS}};
+}
+
+#######################################################################
+###############   Class method to create test objects   ###############
+#######################################################################
+sub test_objects {
+	my ($test_class) = @_;
+	
+	eval "require ".$test_class->class;
+	
+	my @test_objects;
+	
+	push @test_objects, $test_class->class->new(
+		file => 't/sample_data/sample.fastq.gz'
+	);
+
+	return \@test_objects;
+}
+
+1;
+
---
+++ t/Test/MyBio/Data/File/FASTQ/Record.pm
@@ -0,0 +1,98 @@
+package Test::MyBio::Data::File::FASTQ::Record;
+use strict;
+
+use base qw(Test::MyBio);
+use Test::Moose;
+use Test::Most;
+
+#######################################################################
+################   Startup (Runs once in the begining  ################
+#######################################################################
+sub _check_loading : Test(startup => 1) {
+	my ($self) = @_;
+	use_ok $self->class;
+};
+
+#######################################################################
+#################   Setup (Runs before every method)  #################
+#######################################################################
+sub create_new_test_objects : Test(setup) {
+	my ($self) = @_;
+	
+	my $test_class = ref($self) || $self;
+	$self->{TEST_OBJECTS} = $test_class->test_objects();
+};
+
+
+#######################################################################
+###########################   Actual Tests   ##########################
+#######################################################################
+sub _isa_test : Test(1) {
+	my ($self) = @_;
+	isa_ok $self->obj(0), $self->class, "... and the object";
+}
+
+sub name : Test(2) {
+	my ($self) = @_;
+	
+	has_attribute_ok($self->obj(0), 'name', "... test object has the 'name' attribute");
+	is $self->obj(0)->name, 'HWUSI-EAS366_7:1:7:1032#0/1', "... and returns the correct value";
+}
+
+sub sequence : Test(2) {
+	my ($self) = @_;
+	
+	has_attribute_ok($self->obj(0), 'sequence', "... test object has the 'sequence' attribute");
+	is $self->obj(0)->sequence, 'TAATAGTTTTATTTCAGGTATAAGNATC', "... and returns the correct value";
+}
+
+sub quality : Test(2) {
+	my ($self) = @_;
+	
+	has_attribute_ok($self->obj(0), 'quality', "... test object has the 'quality' attribute");
+	is $self->obj(0)->quality, 'BCCB<B@BC@;BBBBB=A;BCBA9%?BB', "... and returns the correct value";
+}
+
+sub extra : Test(2) {
+	my ($self) = @_;
+	
+	has_attribute_ok($self->obj(0), 'extra', "... test object has the 'extra' attribute");
+	is $self->obj(0)->extra, 'something', "... and returns the correct value";
+}
+
+#######################################################################
+##########################   Helper Methods   #########################
+#######################################################################
+sub obj {
+	my ($self, $index) = @_;
+	
+	return $self->{TEST_OBJECTS}->[$index];
+}
+
+sub objs {
+	my ($self) = @_;
+	
+	return @{$self->{TEST_OBJECTS}};
+}
+
+#######################################################################
+###############   Class method to create test objects   ###############
+#######################################################################
+sub test_objects {
+	my ($test_class) = @_;
+	
+	eval "require ".$test_class->class;
+	
+	my @test_objects;
+	
+	push @test_objects, $test_class->class->new({
+		name         => 'HWUSI-EAS366_7:1:7:1032#0/1',
+		sequence     => 'TAATAGTTTTATTTCAGGTATAAGNATC',
+		quality      => 'BCCB<B@BC@;BBBBB=A;BCBA9%?BB',
+		extra        => 'something',
+	});
+
+	return \@test_objects;
+}
+
+1;
