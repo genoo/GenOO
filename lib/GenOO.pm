67398536c228ae723fe5eb34655a39e54363bea3
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2013-11-27 14:04:22 -0500

    Add root GenOO module for documentation

---
+++ lib/GenOO.pm
@@ -0,0 +1,26 @@
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+GenOO - A Modern Perl Framework for High Throughput Sequencing analysis
+
+=head1 DESCRIPTION
+
+    GenOO [jee-noo] is an open-source; object-oriented Perl framework specifically developed for the design
+    of High Throughput Sequencing (HTS) analysis tools. The primary aim of GenOO is to make simple HTS analyses
+    easy and complicated analyses possible. GenOO models biological entities into Perl objects and provides
+    relevant attributes and methods that allow for the manipulation of high throughput sequencing data.
+    Using GenOO as a core development module reduces the overhead and complexity of managing the data and the
+    biological entities at hand. GenOO has been designed to be flexible, easily extendable with modular
+    structure and minimal requirements for external tools and libraries.
+
+=cut
+
+# Let the code begin...
+
+
+package GenOO;
+
+use Modern::Perl;
+
+1;
